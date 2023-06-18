82753b0eb70469e3f9cfd47cc6e2857c247eb9db
anmol860 <thindanmol1234@gmail.com>
2023-06-18 12:33:22 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -110,7 +110,7 @@ export default function Home() {
 
         <div
           className="flex flex-col w-1/2 space-y-2 justify-center mt-8
-      sm:space-y-0 sm:flex-row sm:space-x-4 sm:h-screen"
+      sm:space-y-0 sm:flex-row sm:space-x-4 sm:mb-4cm"
         >
           <button
             onClick={search}
