37711908840d1d700bc741f7147352d4bb8fd3d6
anmol860 <thindanmol1234@gmail.com>
2023-06-18 12:01:09 +0530

    changes

--- components/Footer.js
+++ components/Footer.js
@@ -2,45 +2,31 @@ import { GlobeIcon } from "@heroicons/react/solid";
 
 export const Footer = () => {
   return (
-    <footer
-      className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 mt-auto
-    text-sm text-gray-500
-    "
-    >
-      <div
-        className="px-8 py-3
-        "
-      >
-        <p>United States</p>
-      </div>
-      <div
-        className="grid grid-cols-1 grid-flow-row-dense gap-y-4 px-8 py-3
-        md:grid-cols-2 lg:grid-cols-3"
-      >
-        <div
-          className="flex justify-center items-center
-            md:col-span-2 lg:col-span-1 lg:col-start-2"
-        >
-          <GlobeIcon className="h-5 mr-1 text-green-700" />
-          <span className="">Carbon neutral since 2007</span>
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+        {/* Your main content goes here */}
+      </main>
+      <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
+        <div className="px-8 py-3">
+          <p>United States</p>
         </div>
-        <div
-          className="flex justify-center space-x-8 whitespace-nowrap
-            md:justify-self-start"
-        >
-          <p className="link">Advertising</p>
-          <p className="link">Business</p>
-          <p className="link">How Search works</p>
+        <div className="grid grid-cols-1 grid-flow-row-dense gap-y-4 px-8 py-3 md:grid-cols-2 lg:grid-cols-3">
+          <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
+            <GlobeIcon className="h-5 mr-1 text-green-700" />
+            <span>Carbon neutral since 2007</span>
+          </div>
+          <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
+            <p className="link">Advertising</p>
+            <p className="link">Business</p>
+            <p className="link">How Search works</p>
+          </div>
+          <div className="flex justify-center space-x-8 md:ml-auto">
+            <p className="link">Privacy</p>
+            <p className="link">Terms</p>
+            <p className="link">Settings</p>
+          </div>
         </div>
-        <div
-          className="flex justify-center space-x-8
-            md:ml-auto"
-        >
-          <p className="link">Privacy</p>
-          <p className="link">Terms</p>
-          <p className="link">Settings</p>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 };
